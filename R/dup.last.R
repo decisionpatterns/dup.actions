144b971bdcc1d94df7c3e3cc940fdaeb0f39fbea
Christopher Brown <cbrown@decisionpatterns.com>
2014-04-15 14:01:21 -0700

    Fix comments in dup.last.R

--- R/dup.last.R
+++ R/dup.last.R
@@ -11,13 +11,13 @@ dup.last.default <- function( object, ... )
 dup.last.data.frame <- function( object, ...)  
     object[ ! duplicated(object, fromLast=TRUE ),  ]
 
-#' @export
 
 #' This can be defined with data.table-1.9.3 when duplicated gets the fromLast
 #' argument
 # dup.last.data.table <- function( object, ...)  
 #  object[ ! duplicated(object, fromLast=TRUE ),  ]
 
+#' @export
 dup.last.data.table <- function( object, ...)  {
    revd <- object[ nrow(object):1, ] 
    setkeyv( revd, key(object) )
